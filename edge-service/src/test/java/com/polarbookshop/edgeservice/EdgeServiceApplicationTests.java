d380702a573e228b76335e00710dd73787741b46
Kourosh <kurosh.mozafari@gmail.com>
2023-11-04 15:08:54 +0100

    [edge-service] adding api gateway

---
+++ edge-service/src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,32 @@
+package com.polarbookshop.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class EdgeServiceApplicationTests {
+
+    private static final int REDIS_PORT = 6379;
+
+    @Container
+    private final static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.2.0"))
+            .withExposedPorts(REDIS_PORT);
+
+    @DynamicPropertySource
+    public static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+    }
+
+    @Test
+    void verifyThatSpringContextLoads() {
+    }
+
+}
