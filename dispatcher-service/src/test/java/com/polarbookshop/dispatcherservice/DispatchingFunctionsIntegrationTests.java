a9df08a5787e2736c54be94142a2c95850522f6d
Kourosh <kurosh.mozafari@gmail.com>
2023-11-11 14:28:05 +0100

    [disaptcher-service] update tests

--- dispatcher-service/src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
+++ dispatcher-service/src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.polarbookshop.dispatcherservice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.context.FunctionCatalog;
@@ -16,14 +17,26 @@ public class DispatchingFunctionsIntegrationTests {
     private FunctionCatalog functionCatalog;
 
     @Test
-    void packAndLabelOrder() {
-        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel =
-                functionCatalog.
-                        lookup(Function.class, "pack|label");
+    void packOrder() {
+        Function<OrderAcceptedMessage, Long> pack = functionCatalog.lookup(Function.class, "pack");
+
+        Assertions.assertEquals(12L, pack.apply(new OrderAcceptedMessage(12L)));
+    }
+
+    @Test
+    void labelOrder() {
+        Function<Flux<Long>, Flux<OrderDispatchedMessage>> label =
+                functionCatalog.lookup(Function.class, "label");
         Long orderId = 12L;
-        StepVerifier
-                .create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))
+        StepVerifier.create(label.apply(Flux.just(orderId)))
                 .expectNextMatches(orderDispatchedMessage -> orderDispatchedMessage.orderId().equals(orderId))
                 .verifyComplete();
+    }
+
+    @Test
+    void packAndLabelOrder() {
+        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel = functionCatalog.lookup(Function.class, "pack|label");
+        Long orderId = 12L;
+        StepVerifier.create(packAndLabel.apply(new OrderAcceptedMessage(orderId))).expectNextMatches(orderDispatchedMessage -> orderDispatchedMessage.orderId().equals(orderId)).verifyComplete();
     }
 }
