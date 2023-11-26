e4a0a21932fc0956c0851c304e42fc6db013b99a
Kourosh <kurosh.mozafari@gmail.com>
2023-11-26 23:14:00 +0100

    [order-service] protecting api

---
+++ order-service/src/main/java/com/polarbookshop/orderservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.polarbookshop.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.savedrequest.NoOpServerRequestCache;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(authorize ->
+                        authorize.anyExchange().authenticated())
+                .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()))
+                .requestCache(requestCacheSpec -> requestCacheSpec.requestCache(NoOpServerRequestCache.getInstance()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+}
