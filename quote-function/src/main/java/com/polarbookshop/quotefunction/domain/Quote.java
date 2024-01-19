a647fba06069bb464a82ff9d7e87c4b28c56ca6c
Kourosh <kurosh.mozafari@gmail.com>
2024-01-19 21:21:32 +0100

    [quote-service,quote-function] add modules and commit stage for quote-function

---
+++ quote-function/src/main/java/com/polarbookshop/quotefunction/domain/Quote.java
@@ -0,0 +1,4 @@
+package com.polarbookshop.quotefunction.domain;
+
+public record Quote(String content, String author, Genre genre) {
+}
---
+++ quote-service/src/main/java/com/polarbookshop/quoteservice/domain/Quote.java
@@ -0,0 +1,4 @@
+package com.polarbookshop.quoteservice.domain;
+
+public record Quote(String content, String author, Genre genre) {
+}
